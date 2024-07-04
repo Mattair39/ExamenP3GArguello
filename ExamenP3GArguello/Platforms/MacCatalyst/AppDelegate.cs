d0d5badeba531bfb027dbb1879221cd44babc368
Mattair39 <105869714+Mattair39@users.noreply.github.com>
2024-07-04 07:17:00 -0500

    Add project files.

---
+++ ExamenP3GArguello/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ExamenP3GArguello
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
